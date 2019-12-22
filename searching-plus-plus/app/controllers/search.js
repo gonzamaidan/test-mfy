4dfad242f63abd38c70fd310d7c24fd53d048f6c
Gonzalo Maidan <gonzalo_maidan@hotmail.com>
2019-12-22 18:31:04 -0300

    Agrego filtro y paginado al search

--- searching-plus-plus/app/controllers/search.js
+++ searching-plus-plus/app/controllers/search.js
@@ -1,5 +1,6 @@
 const commons = require('../commons');
 const { get } = commons.repository('properties');
+const Rx = require('rx')
 
 
 module.exports = function searchController(req, res, next) {
@@ -8,11 +9,25 @@ module.exports = function searchController(req, res, next) {
   };
 
   const q = req.query['q'];
-
+  const p = parseInt(req.query['p']) || 1;
+  let cantidadPorPagina = 12;
+  const skip = (p - 1) * cantidadPorPagina
   const onDone = () => res.json(response);
   const onError = (err) => next(err);
   const datasource = get();
-  datasource.take(100).toArray().subscribe((results) => {
+	let filter = property =>
+		property.street.toLowerCase().includes(q.toLowerCase()) ||
+		property.area.toLowerCase().includes(q.toLowerCase()) ||
+		property.county.toLowerCase().includes(q.toLowerCase()) ||
+		property.province.toLowerCase().includes(q.toLowerCase());
+	const count = datasource.count(filter);
+	const results = datasource
+		.filter(filter
+      )
+        .skip(skip)
+      .take(cantidadPorPagina).toArray();
+	Rx.Observable.combineLatest(count, results,(count, results) => ({count, results})).subscribe(({count, results}) => {
+    response.count = count
     response.results = results
   }, onError, onDone);
 };
