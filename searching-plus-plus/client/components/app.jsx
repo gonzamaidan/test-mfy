fe152e0e73c6e89676ba04233747595a70d7c618
Gonzalo Maidan <gonzalo_maidan@hotmail.com>
2019-12-22 18:33:18 -0300

    Muevo logica de busqueda al padres y agrego pagination

--- searching-plus-plus/client/components/app.jsx
+++ searching-plus-plus/client/components/app.jsx
@@ -4,39 +4,94 @@ import { connect } from 'react-redux';
 import Searchbox from './searchbox.jsx';
 import Resultbox from './resultbox.jsx';
 import Title from './title.jsx';
-import { Grid, CssBaseline, AppBar, Toolbar, Typography } from '@material-ui/core'
+import { Grid, CssBaseline, TablePagination } from '@material-ui/core'
 
 import '../styles/app.css';
+import ACTIONS from "../actions";
 
 class App extends Component {
-    render() {
-        return (
-            <Grid containter>
-                <CssBaseline/>
-                <Title/>
-                <Grid className="client-styles-app-bodyGrid" item xs={12} container>
-                    <Grid xs={12} item>
-                        <Searchbox />
-                    </Grid>
-                    <Grid xs={12} item>
-                        <Resultbox results={this.props.results} />
-                    </Grid>
-                </Grid>
-            </Grid>
-        );
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			rowsPerPage:12,
+			page: 1,
+		}
+	}
+	
+	componentDidMount() {
+		this.setState({ text: '' });
+	}
+	
+	
+	
+	render() {
+		const handleChangePage = (event, newPage) => {
+			this.props.doSearch(this.state.text, newPage + 1)
+			this.setState({page: newPage + 1});
+		}
+		const search = () => {
+			this.props.doSearch(this.state.text, 1);
+			this.setState({page:1})
+        }
+		
+		const handleTextChange = (event) => {
+			this.setState({ text: event.target.value });
+		}
+		
+		const {page, rowsPerPage} = this.state
+		return (
+			<Grid container
+				  direction="row"
+				  justify="center"
+				  alignItems="center"
+			>
+				<CssBaseline/>
+				<Title/>
+				<Grid className="client-styles-app-bodyGrid" item xs={12} sm={10}>
+					<Grid xs={12} item>
+						<Searchbox handleTextChange={handleTextChange} search={search} />
+					</Grid>
+					<Grid xs={12} item>
+						{
+							this.props.results.length > 0 &&
+							(
+								<TablePagination
+									rowsPerPageOptions={[10]}
+									component="div"
+									count={this.props.count}
+									rowsPerPage={rowsPerPage}
+									page={page - 1}
+									backIconButtonProps={{
+										'aria-label': 'Pagina anterior',
+									}}
+									nextIconButtonProps={{
+										'aria-label': 'Proxima pagina',
+									}}
+									onChangePage={handleChangePage}
+								/>)}
+						<Resultbox results={this.props.results} />
+					</Grid>
+				</Grid>
+			</Grid>
+		);
+	}
 }
 
 
 const mapStateToProps = (state, ownProps) => {
-    return {
-        results: state.results,
-        text: state.text,
-    };
+	return {
+		results: state.results,
+		count: state.count,
+		text: state.text,
+		page: state.page,
+		rowsPerPage: state.rowsPerPage
+	};
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-    return {};
+	return {
+		doSearch: (text, page) => dispatch(ACTIONS.searchFor(text, page))
+	};
 }
 
 
--- searching-plus-plus/client/components/searchbox.jsx
+++ searching-plus-plus/client/components/searchbox.jsx
@@ -1,43 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import ACTIONS from '../actions';
-import { Grid, Paper } from '@material-ui/core'
-
+import { Grid, Paper, TextField, Button } from '@material-ui/core'
+import "../styles/searchbox.css"
 
 /**
  * We keep the UI state local, decoupled from the *REDUX STATE*.
  */
 
 class Searchbox extends Component {
-    handleTextChange(event) {
-        this.setState({ text: event.target.value });
-    }
-
-    componentDidMount() {
-        this.setState({ text: '' });
-    }
-
-    render() {
-        return (
-            <Paper className="searchbox">
-                <Grid container>
-                    <input type="text" onChange={event => this.handleTextChange(event)} />
-                    <button onClick={() => this.props.doSearch(this.state.text)}>Search</button>
-                </Grid>
-            </Paper>
-        );
-    }
+	render() {
+		return (
+			<Paper className="client-styles-searchbox-paper">
+				<Grid container spacing={2}>
+					<Grid item xs={10} sm={4} lg={2}>
+						<TextField
+							id="standard-name"
+							label="Buscar"
+							onChange={event => this.props.handleTextChange(event)}
+							margin="normal"
+						/>
+					</Grid>
+					<Grid item xs={2}>
+						<Button variant={"contained"} onClick={() => this.props.search()}>Buscar</Button>
+					</Grid>
+				</Grid>
+			</Paper>
+		);
+	}
 }
 
 
 const mapStateToProps = (state, ownProps) => {
-    return {};
+	return {};
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-    return {
-        doSearch: (text) => dispatch(ACTIONS.searchFor(text))
-    };
+	return {};
 }
 
 
