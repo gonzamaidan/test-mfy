88569efe5257f1fdb3c85035f59ae2677314da75
Gonzalo Maidan <gonzalo_maidan@hotmail.com>
2019-12-22 22:50:55 -0300

    Modifico index

--- parse-large-feeds/src/index.js
+++ parse-large-feeds/src/index.js
@@ -1,12 +1,12 @@
-import http from 'http';
 import { createLogger } from 'bunyan';
+import fs from 'fs'
 
 const logger = createLogger({name: 'feedparser'});
+const feedParser = require('./controllers/parse-feed')
+const cache = require('./controllers/cache')
+if (!fs.existsSync('./out')){
+	fs.mkdirSync('./out');
+}
 
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-
-logger.info('Server running at http://127.0.0.1:1337/');
+cache(() => feedParser("./src/cache/cache.xml", {}), {url: 'https://panel.siprop.com/propiedades/export/id/utq0yl2wda'})
 
